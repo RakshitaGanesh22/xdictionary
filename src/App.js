37570cba3fb16f6cc88551a48f1b99cf39394097
RakshitaGanesh22 <rakshitaganesh705@gmail.com>
2024-09-07 20:27:10 +0530

    done with excicution of dictionary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  let Dictionary=
+  [
+  
+      { word: "React", meaning: "A JavaScript library for building user interfaces." },
+  
+      { word: "Component", meaning: "A reusable building block in React." },
+  
+      { word: "State", meaning: "An object that stores data for a component." }
+  
+  ]
+  
+  let[value,setValue]=useState("");
+  let[Defination,setDefine]=useState("");
+  function HandleChange(event){
+    setValue(event.target.value);
+  }
+  function HandleClick(){
+    setDefine("");
+    let flag=0;
+    for(let i=0;i<Dictionary.length;i++){
+      if(Dictionary[i].word.toLowerCase()==value.toLowerCase()){
+        setDefine(Dictionary[i].meaning);
+        flag=1;
+      }
+
+    }
+    if(flag!==1){
+      setDefine('Word not found in the dictionary.');
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={HandleChange}/>
+      <button type="submit" onClick={HandleClick}>Search</button>
+      <h3>Defination:</h3><br/>
+      <p>{Defination}</p>
+
     </div>
   );
 }
